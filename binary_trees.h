934d204b70fbcfc41d773fb21a58ca8650690fb2
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-29 07:03:28 -0700

    fix betty

--- binary_trees.h
+++ binary_trees.h
@@ -4,12 +4,11 @@
 /* Structure for a binary tree node */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
 /* Function prototypes */
