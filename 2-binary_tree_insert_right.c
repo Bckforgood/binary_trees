b44d9a2637ddc2224ca56bcceb57a1e964e0f888
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-30 15:40:11 -0700

    inorder

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,10 +12,10 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_right = binary_tree_node(parent, value);
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_right = binary_tree_node(parent, value);
 
 	if (new_right == NULL)
 		return (NULL);
