7926d15df976d5f58320a1651e5cd0085ea8157e
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-30 15:06:16 -0700

    fix error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -6,29 +5,24 @@
  * @parent: Pointer to the node to insert the left-child in.
  * @value: Value to store in the new node.
  *
- * Return: Pointer to the created node,
- * or NULL on failure or if parent is NULL.
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	/* Check if parent is NULL */
-	if (parent == NULL)
-		return (NULL);
+    if (parent == NULL)
+        return NULL;
 
-	/* Create a new left child node */
-	binary_tree_t *new_left = binary_tree_node(parent, value);
+    binary_tree_t *new_node = binary_tree_node(parent, value);
+    if (new_node == NULL)
+        return NULL;
 
-	if (new_left == NULL)
-		return (NULL);
+    if (parent->left != NULL)
+    {
+        new_node->left = parent->left;
+        parent->left->parent = new_node;
+    }
 
-	/* If parent already has a left child, rearrange nodes */
-	if (parent->left != NULL)
-	{
-		new_left->left = parent->left;
-		parent->left->parent = new_left;
-	}
-
-	/* Set new left child and return */
-	parent->left = new_left;
-	return (new_left);
+    parent->left = new_node;
+    return new_node;
 }
+
